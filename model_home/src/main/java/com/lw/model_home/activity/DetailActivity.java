f44d63e7da420414eebaebdbecdb6b9cbfe623c8
陆威 <404474566@qq.com>
2021-05-23 00:03:23 +0800

    ceshishiyong

---
+++ model_home/src/main/java/com/lw/model_home/activity/DetailActivity.java
@@ -0,0 +1,14 @@
+package com.lw.model_home.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by lw on 2021/5/23.
+ */
+
+public class DetailActivity extends Activity {
+    private String home = "123212";
+    private String home1 = "123212";
+    private String home2 = "123212";
+    private String home3 = "123212";
+}
